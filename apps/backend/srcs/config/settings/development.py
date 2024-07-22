90d88a3da51b41c92aa2314c0b64f177b6041230
lim <gylim@student.42seoul.kr>
2024-07-22 22:12:49 +0900

    modify django version

--- apps/backend/srcs/config/settings/development.py
+++ apps/backend/srcs/config/settings/development.py
@@ -13,15 +13,21 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+import environ
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Take environment variables from .env file
+env = environ.Env()
+# environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
+environ.Env.read_env(env_file=BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&&$1y6%2!f%*b)1)fj9m)p59@*m5ml-(rdmv)ooio18^#)m$m8"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
