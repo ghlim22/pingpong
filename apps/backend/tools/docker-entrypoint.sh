d5e9a2d1fb47c9169b87c8a2c5e5ca62628a0d8a
lim <gylim@student.42seoul.kr>
2024-08-03 17:22:10 +0900

    [ADD] https connection

--- apps/backend/tools/docker-entrypoint.sh
+++ apps/backend/tools/docker-entrypoint.sh
@@ -3,4 +3,5 @@
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
 
-exec gunicorn --bind 0.0.0.0:8000 config.wsgi:application
+#exec gunicorn --bind 0.0.0.0:8000 config.wsgi:application
+exec gunicorn --certfile=gylim.pem --keyfile=gylim.key --bind 0.0.0.0:443 config.wsgi:application
