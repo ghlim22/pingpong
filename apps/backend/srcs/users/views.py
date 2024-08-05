80b1db652619c8778738d8c0e9ccc5024d58e6ba
lim <gylim@student.42seoul.kr>
2024-08-05 14:47:05 +0900

    [ADD] SignUpView

--- apps/backend/srcs/users/views.py
+++ apps/backend/srcs/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from apps.backend.srcs.users.models import CustomUser
+from apps.backend.srcs.users.serializers import SignUpSerializer
 
 # Create your views here.
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = SignUpSerializer
