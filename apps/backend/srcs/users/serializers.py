3891c3a33d11ba3a81570935268b7499f1cdf93b
lim <gylim@student.42seoul.kr>
2024-08-05 14:38:27 +0900

    [ADD] user register serializer

---
+++ apps/backend/srcs/users/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
+
+# Create your tests here.
+from .models import CustomUser
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=CustomUser.objects.all())],
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+    )
+    confirm_password = serializers.CharField(
+        write_only=True,
+        required=True,
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ["email", "password", "confirm_password"]
+
+    def validate(self, data):
+        if data["password"] != data["confirm_password"]:
+            raise serializers.ValidationError({"confirm_password": "Password didn't match"})
+        return data
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
+        token = Token.objects.create(user=user)
+        return user
