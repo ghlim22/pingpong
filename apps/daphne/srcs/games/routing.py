4158602de215f8a542a1af026f2db698352a8728
lim <gylim@student.42seoul.kr>
2024-08-13 20:02:25 +0900

    delete dephane

--- apps/daphne/srcs/games/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-
-from . import gameConsumer, gameQueueConsumer
-
-websocket_urlpatterns = [
-    re_path(r"^wss/games/rankgames/(?P<type>[\w-]+)/$", gameQueueConsumer.RankGameRoomConsumer.as_asgi()),
-    re_path(r"^wss/games/start/(?P<game_id>[a-fA-F0-9\-]{36})/(?P<type>[\w-]+)/$", gameConsumer.GameConsumer.as_asgi()),
-]
--- apps/daphne/tools/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate
-python3 manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL --nickname $DJANGO_SUPERUSER_NICKNAME
-#exec gunicorn --bind 0.0.0.0:8000 config.wsgi:application
-#exec gunicorn --certfile=gylim.pem --keyfile=gylim.key --bind 0.0.0.0:443 config.wsgi:application
-
-exec daphne -e ssl:443:privateKey=gylim.key:certKey=gylim.pem config.asgi:application
