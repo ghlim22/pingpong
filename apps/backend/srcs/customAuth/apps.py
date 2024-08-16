41fb6ba4d4b19c3b88765650f701f0587c359b2a
lim <gylim@student.42seoul.kr>
2024-08-16 17:26:17 +0900

    add customAuth app

---
+++ apps/backend/srcs/customAuth/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class CustomauthConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "customAuth"
+
+    def ready(self):
+        pass
