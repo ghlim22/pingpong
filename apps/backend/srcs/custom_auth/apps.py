6aae6c3f5b9942598c97f3245cda9d9949e440a7
lim <gylim@student.42seoul.kr>
2024-08-20 12:00:19 +0900

    erase comment

--- apps/backend/srcs/custom_auth/apps.py
+++ apps/backend/srcs/custom_auth/apps.py
@@ -7,11 +7,4 @@ class CustomAuthConfig(AppConfig):
     name = "custom_auth"
 
     def ready(self):
-        # from channels.db import database_sync_to_async
-        # from django.contrib.auth.models import AnonymousUser
-        # from django.core.exceptions import ObjectDoesNotExist
-        # from rest_framework.authtoken.models import Token
-        # from urllib.parse import parse_qs
         import custom_auth.signals
-
-        # django.setup()
