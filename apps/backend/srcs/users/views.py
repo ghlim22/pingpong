8e7bf3c4087b473773f1e1c356f812c6c7250141
lim <gylim@student.42seoul.kr>
2024-08-23 17:20:25 +0900

    modify CurrentUserAPI's picture return value type

--- apps/backend/srcs/users/views.py
+++ apps/backend/srcs/users/views.py
@@ -72,7 +72,11 @@ class UserCurrentAPIView(generics.RetrieveUpdateDestroyAPIView):
     def retrieve(self, request, *args, **kwargs):
         instance = request.user
         serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+
+        data = serializer.data
+        data["picture"] = instance.picture.url
+
+        return Response(data)
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop("partial", False)
@@ -86,7 +90,10 @@ class UserCurrentAPIView(generics.RetrieveUpdateDestroyAPIView):
             # forcibly invalidate the prefetch cache on the instance.
             instance._prefetched_objects_cache = {}
 
-        return Response(serializer.data)
+        data = serializer.data
+        data["picture"] = instance.picture.url
+
+        return Response(data)
 
     def destroy(self, request, *args, **kwargs):
         instance = request.user
