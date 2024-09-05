c6a6b6af2c145d7e874f5ee4c7bf711f778d40a1
lim <gylim@student.42seoul.kr>
2024-09-05 12:04:33 +0900

    Move static url addtion

--- apps/backend/srcs/config/urls.py
+++ apps/backend/srcs/config/urls.py
@@ -29,11 +29,11 @@ urlpatterns = [
     path("api/admin/", admin.site.urls),
     path("api/auth/", include("custom_auth.urls")),
     path("api/users/", include("users.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns += [
         path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
         path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
         path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    ]
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
